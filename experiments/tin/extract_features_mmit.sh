fcd0c16d245be95f85a7805ed4f326a9eee72bfa
kshitijd20 <kshitijdwivedi93@gmail.com>
2020-10-30 15:14:48 +0100

    Feature extraction configs for TIN

---
+++ experiments/tin/extract_features_mmit.sh
@@ -0,0 +1,8 @@
+T=`date +%m%d%H%M`
+ROOT=../..
+cfg=mmit.yaml
+ad='/scratch/kshitijd/Algonauts2020/activations/TIN_MMIT'
+
+export PYTHONPATH=$ROOT:$PYTHONPATH
+
+python $ROOT/x_temporal/extract_activations.py --config $cfg --activations_dir $ad | tee log.test.$T
